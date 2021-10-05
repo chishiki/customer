07bc8a85a69ef27d950c0ce26b896f9f5af1be19
Christopher Webb <chishiki@gmail.com>
2021-10-05 12:27:09 +0900

    stub out customer sql

---
+++ data/customer-schema.sql
@@ -0,0 +1,23 @@
+
+DROP TABLE IF EXISTS `order_Customer`;
+
+CREATE TABLE `order_Customer` (
+  `customerID` int(12) NOT NULL AUTO_INCREMENT,
+  `siteID` int(12) NOT NULL,
+  `creator` int(12) NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  `deleted` int(1) NOT NULL,
+  `customerNameEnglish` varchar(100) NOT NULL,
+  `customerNameJapanese` varchar(100) NOT NULL,
+  `customerPersonInCharge` varchar(100) NOT NULL,
+  `customerRepresentativeDepartment` varchar(100) NOT NULL,
+  `customerRepresentativeTitle` varchar(100) NOT NULL,
+  `customerHonorarySuffix` varchar(10) NOT NULL,
+  `customerNotes` varchar(255) NOT NULL,
+  `customerTelephone` varchar(50) NOT NULL,
+  `customerFax` varchar(50) NOT NULL,
+  `customerEmail` varchar(50) NOT NULL,
+  `priceLevel` int(2) NOT NULL,
+  PRIMARY KEY (`customerID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
